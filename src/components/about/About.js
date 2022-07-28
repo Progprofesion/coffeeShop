47c95f6c2c990dbe7f8b75e4c81304d9b7edc09e
Dmitriy <tiemposilencioo@gmail.com>
2022-07-28 19:33:20 +0300

    footer

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,16 +1,13 @@
 import "./about.scss"
 import "../../styles/fontSize.scss";
 import coffeeBeansIconBlack from "./img/coffeeBeansIconBlack.svg";
+import IconLine from "../iconLine/IconLine";
 
 const About = () => {
     return (
         <div className="about">
             <h2 className="about__title">About Us</h2>
-            <div className="about__item">
-                <div className="about__item-line"></div>
-                <img src={coffeeBeansIconBlack} alt="coffeeBeans" />
-                <div className="about__item-line"></div>
-            </div>
+            <IconLine />
             <h3 className="about__descr fz-14">Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
                 Afraid at highly months do things on at. Situation recommend objection do intention
                 so questions. As greatly removed calling pleased improve an. Last ask him cold feel

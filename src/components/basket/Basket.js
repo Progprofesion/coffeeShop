6943f99343c3b9167afd12cd17934791b6cd5271
Dmitriy <tiemposilencioo@gmail.com>
2022-08-25 20:31:07 +0300

    basketTotal

--- src/components/basket/Basket.js
+++ src/components/basket/Basket.js
@@ -9,38 +9,24 @@ const Basket = () => {
     const state = useSelector(state => state.basket.stateBasket);
 
     const [amount, setAmount] = useState(-1);
-    const [count, setCount] = useState(0);
     const [total, setTotal] = useState(0);
-
-
 
     useEffect(() => {
         setAmount(amount + 1)
         if (state.price !== undefined) {
-            let test = state.price.replace(/\$/, '');
-            let number = test * 1;
-            setCount(number);
-        }
-        if (count) {
-            let res = count + count
-            setCount(res);
-            console.log(res)
+            // Убирать символ $ преобразовать в число и записать в стейт.
+            // setTotal((state.price.replace(/\$/, '') * 1) + total);
+            setTotal((state.price.replace(/\$/, '') * 1) + total);
+
         }
         // eslint-disable-next-line
     }, [state.count]);
-    // console.log(count)
-
-
-    // useEffect(() => {
-    //     setCount(number)
-    // }, [amount])
-
-
+    console.log(total)
 
     return (
         <div className="basket">
             <div className="basket__res">{amount}</div>
-            <div className="basket__price">{`${count}$`}</div>
+            <div className="basket__price">{`${total}$`}</div>
         </div>
     )
 };

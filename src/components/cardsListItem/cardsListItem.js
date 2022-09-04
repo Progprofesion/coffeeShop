750beba30fb689f5143f289942d90b1f27ff6400
Dmitriy <tiemposilencioo@gmail.com>
2022-09-04 17:24:34 +0300

    basketCardsReview

--- src/components/cardsListItem/cardsListItem.js
+++ src/components/cardsListItem/cardsListItem.js
@@ -8,12 +8,10 @@ import 'animate.css';
 import './cardsListItem.scss';
 
 
-const Cards = ({ page, img, title, country, price }) => {
+const Cards = ({ id, img, title, country, price }) => {
 
     const stateCards = useSelector(state => state.basket.basketCards);
     const state = useSelector(state => state.basket.stateBasket);
-
-
 
     const [{ items }, setItems] = useState({ items: [] });
     const [count, setCount] = useState(Math.floor(Math.random() * 1000));
@@ -26,44 +24,39 @@ const Cards = ({ page, img, title, country, price }) => {
         e.preventDefault();
         setCount(count + 1)
         setAmount(amount + 1)
-        dispatch(activeStateBasket({ page, img, price, title, country, count, amount }))
-        console.log(stateCards.length)
+        dispatch(activeStateBasket({ id, img, price, title, country, count, amount }))
+        if (stateCards === undefined) {
+            console.log(stateCards.key)
+        }
     };
 
     useEffect(() => {
-        if (state.amount === 0 || stateCards.length > 1) {
+        if (state.amount === 0) {
             setKey(key + 1)
-            addItem(key)
+            addItem(key, id)
         }
-
         // eslint-disable-next-line
     }, [state.count]);
 
     const basketDecr = (e) => {
         e.preventDefault();
-        setCount(count + 1)
+        // setCount(count + 1)
         if (amount > 0) {
             setAmount(amount - 1)
-            dispatch(activeStateBasket({ page, img, price, title, country, count, amount }))
+            dispatch(activeStateBasket({ id, img, price, title, country, amount }))
         }
-        // let res = items.filter(item => item.key > key)
-        // setItems({ items: [...items.slice(1)] })
         dispatch(activeTotals({ count, amount }))
-
         // Убавляет на 1
-        // dispatch(activeBasketCards([...stateCards.slice(1)]))
-        dispatch(activeBasketCards([...stateCards.filter((item, i) => {
-            console.log(item, i)
-            return item.key === item.i
-        })]))
+        dispatch(activeBasketCards([...stateCards.slice(1)]))
+        // dispatch(activeBasketCards([...stateCards.filter((item, i) => {
+        //     console.log(item, i)
+        //     return item.key === item.i
+        // })]))
         // setItems([...stateCards.slice(1)])
-
-
     };
 
-    console.log(stateCards)
-    const addItem = async (index) => {
-        items.push(<div key={index} className="basketLayont__wrapp">
+    const addItem = async (index, id) => {
+        items.push(<div key={index} id={id} className="basketLayont__wrapp">
             <img src={state.img} alt="coffee" className="basketLayont__img" />
             <div className="basketLayont__result">
                 <div className="basketLayont__title">{state.title}</div>
@@ -71,10 +64,8 @@ const Cards = ({ page, img, title, country, price }) => {
                 <div className="basketLayont__price">{state.price}</div>
             </div>
         </div>);
-        if (items !== undefined) {
-            // setItems({ items })
-            dispatch(activeBasketCards([...items]))
-        }
+        setItems({ items: [...items] })
+        dispatch(activeBasketCards([...items]))
     };
 
     return (
@@ -82,7 +73,7 @@ const Cards = ({ page, img, title, country, price }) => {
             name='cards'
             className="cards animate__animated animate__flipInX">
             <div className="cards__item" name='cards' type="text">
-                <Link to={`/ourcoffee/${page}`}>
+                <Link to={`/ourcoffee/${id}`}>
                     <img className="cards__img" src={img} alt="coffee" />
                 </Link>
             </div>

deb85c32250a9f1f2b4a7dac34c7bade3ce8d3fc
Dmitriy <tiemposilencioo@gmail.com>
2022-11-15 10:32:51 +0300

    fixHamburger

--- src/components/hamburger/Hamburger.js
+++ src/components/hamburger/Hamburger.js
@@ -27,6 +27,7 @@ const Burger = () => {
                     <span></span>
                     <span></span>
                     <span></span>
+                    <span></span>
                 </div>
             </div>
             <div

81a7986efc39422ac57e722fa95ffaa0a9171bd8
Dmitriy <tiemposilencioo@gmail.com>
2022-12-10 18:12:03 +0300

    swiper

--- src/components/swiper/Sddwiper.js
+++ src/components/swiper/Sddwiper.js
@@ -1,32 +1,88 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import 'swiper/scss';
-import './swap.scss';
+// import 'swiper/scss';
+// import './swap.scss';
+// import 'swiper/css/mousewheel';
 
-import AROMISTICOCoffee1kg from "src/assets/cardsImg/AROMISTICOCoffee1kg.svg";
-import EvaDiaKenyaGrinders from "src/assets/cardsImg/EvaDiaKenyaGrinders.jpg"
-import LavazzaTierra1KG from "src/assets/cardsImg/LavazzaTierra1KG.jpg";
-import LORCremaAbsoluClassique from 'src/assets/cardsImg/LORCremaAbsoluClassique.webp';
-import PrestoCoffeeBeans1kg from "src/assets/cardsImg/PrestoCoffeeBeans1kg.svg";
-import SolimoCooffeeBeans2kg from "src/assets/cardsImg/SolimoCooffeeBeans2kg.svg";
 
-export default () => {
+import lavazza from 'src/assets/cardsImg/lavazza.jpg';
+import leboCenia from "src/assets/cardsImg/leboCenia.jpg";
+import lavazzaColumbia from "src/assets/cardsImg/lavazzaColumbia.jpg";
+import egoiste from "src/assets/cardsImg/egoiste.jpg";
+import bushidoColumbia from "src/assets/cardsImg/bushidoColumbia.jpg";
+import bushido from "src/assets/cardsImg/bushido.jpg";
+
+// export default () => {
+//     return (
+//         <div className="swap">
+//             <Swiper
+//                 // install Swiper modules
+//                 modules={[Navigation, Pagination, Scrollbar, A11y]}
+//                 spaceBetween={50}
+//                 mousewheel
+//                 slidesPerView={1}
+//                 navigation
+//                 centeredSlides={true}
+//                 loop={true}
+//                 pagination={{ clickable: true }}
+//                 scrollbar={{ draggable: true }}
+//                 onSwiper={(swiper) => console.log(swiper)}
+//                 onSlideChange={() => console.log('slide change')}
+//             >
+
+//                 <SwiperSlide><img className="swapImg" src={lavazza} alt="coffee" /></SwiperSlide>
+//                 <SwiperSlide><img className="swapImg" src={leboCenia} alt="coffee" /></SwiperSlide>
+//                 <SwiperSlide><img className="swapImg" src={lavazzaColumbia} alt="coffee" /></SwiperSlide>
+//                 <SwiperSlide><img className="swapImg" src={egoiste} alt="coffee" /></SwiperSlide>
+//                 <SwiperSlide><img className="swapImg" src={bushidoColumbia} alt="coffee" /></SwiperSlide>
+//                 <SwiperSlide><img className="swapImg" src={bushido} alt="coffee" /></SwiperSlide>
+//             </Swiper>
+//         </div>
+//     );
+// };
+
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+export default function App() {
     return (
-        <div className="swap">
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                <SwiperSlide><img className="swapImg" src='https://about-tea.ru/wp-content/uploads/d/4/1/d4103f7ca3583352ce2078b29e0fa262.jpeg' alt="coffee" /></SwiperSlide>
-                <SwiperSlide><img className="swapImg" src={EvaDiaKenyaGrinders} alt="coffee" /></SwiperSlide>
-                <SwiperSlide><img className="swapImg" src='https://ra-karandash.ru/wp-content/uploads/2017/10/%D0%A3%D0%BF%D0%B0%D0%BA%D0%BE%D0%B2%D0%BA%D0%B0-%D0%BA%D0%BE%D1%84%D0%B5_Parrada_5.jpg' alt="coffee" /></SwiperSlide>
-                <SwiperSlide><img className="swapImg" src={LORCremaAbsoluClassique} alt="coffee" /></SwiperSlide>
-                <SwiperSlide><img className="swapImg" src="http://micahhansen.com/wp-content/uploads/2017/01/Fiddlers-Coffee-bag-01.jpg" alt="coffee" /></SwiperSlide>
-                <SwiperSlide><img className="swapImg" src="https://kofe78.ru/image/cache/data/Vergnano_new/Vergnano%20Espresso%201%20kg-800x800.jpg" alt="coffee" /></SwiperSlide>
-            </Swiper>
-        </div>
+
+        <>
+            <div className="container">
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={30}
+                    loop={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide><img className="swapImg" src={lavazza} alt="coffee" /></SwiperSlide>
+                    <SwiperSlide><img className="swapImg" src={leboCenia} alt="coffee" /></SwiperSlide>
+                    <SwiperSlide><img className="swapImg" src={lavazzaColumbia} alt="coffee" /></SwiperSlide>
+                    <SwiperSlide><img className="swapImg" src={egoiste} alt="coffee" /></SwiperSlide>
+                    <SwiperSlide><img className="swapImg" src={bushidoColumbia} alt="coffee" /></SwiperSlide>
+                    <SwiperSlide><img className="swapImg" src={bushido} alt="coffee" /></SwiperSlide>
+                </Swiper>
+            </div>
+        </>
     );
-};
+}
